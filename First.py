61c323f7aedafa83860a8c8a905703d48201be33
Jhansipeketi <99330300+Jhansipeketi@users.noreply.github.com>
2022-02-10 19:38:32 +0530

    Create First.py

---
+++ First.py
@@ -0,0 +1,19 @@
+sno=int(input("enter the sno:"))
+sname=str(input("ente the sname:"))
+sgroup=str(input("ente the sgroup:"))
+s1=float(input("maths marks:"))
+s2=float(input("physics marks:"))
+s3=float(input("computer science marks:"))
+if s1>=35 and s2>=32 and s3>=35:
+    print("pass")
+else:
+    print("fail")
+
+Output:
+ enter the sno:34
+ente the sname:jhansi
+ente the sgroup:mpcs
+maths marks:80
+physics marks:90
+computer science marks:90
+pass
