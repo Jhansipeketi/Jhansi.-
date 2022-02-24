39ed4832d08aa494efad480c81ede5b504ec4625
Jhansipeketi <99330300+Jhansipeketi@users.noreply.github.com>
2022-02-24 08:36:00 +0530

    Create Max and min.py

---
+++ Max and min.py
@@ -0,0 +1,12 @@
+ n=int(input())
+l=[]
+for i in range(n):
+    x=int(input())
+    l.append(x)
+print('the minimum number is:',min(l))
+print('the maximum number is:',max(l))
+[23/02, 20:22] +91 99635 94185: 2
+35
+42
+the minimum number is: 35
+the maximum number is: 42
